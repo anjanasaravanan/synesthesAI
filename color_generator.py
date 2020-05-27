9acdbc9a6c444570dfed07a96966f154f03c3a40
affengoettin <65838949+affengoettin@users.noreply.github.com>
2020-05-27 00:32:37 -0700

    Update color_generator.py

--- color_generator.py
+++ color_generator.py
@@ -16,19 +16,16 @@ def generate_swath(rgb_vector):
     return swath
 
 
-#colors = []
-#for r in range(0, 255, 10):
-#    for g in range(0, 255, 10):
-#        for b in range(0, 255, 10):
-#            new_color = [r/255, g/255, b/255]
-#            new_swath = generate_swath(new_color)
-#            colors.append(new_swath)
+colors = []
+for r in range(0, 255, 10):
+    for g in range(0, 255, 10):
+        for b in range(0, 255, 10):
+            new_color = [r/255, g/255, b/255]
+            new_swath = generate_swath(new_color)
+            colors.append(new_swath)
 
-#np.save('colors', colors)
+np.save('colors', colors)
 # number of colors generated: 17576
-
-#colors = np.load('colors.npy')
-#num_colors = len(colors)
 
 
 def generate_palette(rgb_vectors):
@@ -45,24 +42,18 @@ def generate_palette(rgb_vectors):
                 palette[i][j] = rgb_vectors[2]
             else:
                 palette[i][j] = rgb_vectors[3]
-
-
     return palette
 
 
 
-#palettes = []
-#
-#for i in range(num_colors):
-#    rgb_vectors = []
-#    for j in range(4):
-#        rgb_vectors.append(random.choice(colors)[0][0])
-#    new_palette = generate_palette(rgb_vectors)
-#    palettes.append(new_palette)
+palettes = []
 
-#print('Number of palettes generated is', len(palettes))
-#np.save('palettes', palettes)
+for i in range(num_colors):
+    rgb_vectors = []
+    for j in range(4):
+        rgb_vectors.append(random.choice(colors)[0][0])
+    new_palette = generate_palette(rgb_vectors)
+    palettes.append(new_palette)
+    
+np.save('palettes', palettes)
 
-palettes = np.load('palettes.npy')
-plt.imshow(palettes[50])
-plt.show()
