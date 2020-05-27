2dbcd95579838eecde79cf2bbe682d4aec339ca6
affengoettin <65838949+affengoettin@users.noreply.github.com>
2020-05-27 00:35:22 -0700

    Update my_gan.py

--- my_gan.py
+++ my_gan.py
@@ -1,22 +1,3 @@
-"""Misadventures with GAN, resume!"""
-
-
-
-"""
-Notes:
-- currently training on: SWATHS.
-- current noise input: RANDOM.
-
-Notes on results:
-- swath training, no convergence even after 150 epochs, trying 200
-- 200: seeing convergence. trying 250!
-
-Current:
-- training the generator on raw (normalized?) wav data rather than random
-    normalized 'noise'
-
-
-"""
 import tensorflow as tf
 from tensorflow.keras import layers
 import time # w o w
@@ -91,17 +72,8 @@ def make_discriminator_model():
 
 generator = make_generator_model()
 
-#noise = tf.random.normal([1, noise_dim])
-#generated_image = generator(noise, training=False)
-
-
-#generated_image = tf.linalg.normalize(generated_image, axis=3)[0]
-#generated_image = tf.math.abs(generated_image)
-#plt.imshow(generated_image[0])
-#plt.show()
-
 discriminator = make_discriminator_model()
-#decision = discriminator(enhanced_image)
+
 
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
@@ -162,14 +134,14 @@ def train(dataset, epochs):
 
         print('Time for epoch {} is {} seconds.'.format(epoch+1, time.time()-start))
 
-    generator.save_weights('SAVED_WEIGHTS/generator')
+    generator.save_weights('SAVED_WEIGHTS')
 
     display.clear_output(wait=True)
     generate_and_save_images(generator, epochs, seed)
 
 def generate_and_save_images(model, epoch, test_input):
     predictions = model(test_input, training=False)
-    predictions = tf.linalg.normalize(predictions, axis=3)[0] # write this function.
+    predictions = tf.linalg.normalize(predictions, axis=3)[0] 
     predictions = tf.math.abs(predictions)
 
     fig = plt.figure(figsize=(4, 4))
@@ -182,24 +154,8 @@ def generate_and_save_images(model, epoch, test_input):
     plt.savefig('training_examples/image_at_epoch_{:04}.png'.format(epoch))
     plt.close()
 
-#print('TRAINING INITIALIZED.')
-#train(train_dataset, EPOCHS)
-
-def music_tings(new_noise, loaded_gen):
-
-    temp_result = loaded_gen(new_noise)
-    edited_result = tf.linalg.normalize(temp_result, axis=3)[0]
-    edited_result = tf.math.abs(edited_result)
-
-    elem_counter = 0
-    for elem in edited_result:
-        elem_counter += 1
-
-        fig = plt.figure()
-        plt.imshow(elem)
-        plt.axis('off')
-        plt.savefig('test/images/image_elem_{:03}.png'.format(elem_counter))
-        plt.close()
+print('TRAINING INITIALIZED.')
+train(train_dataset, EPOCHS)
 
 def post_processing():
 
@@ -210,7 +166,7 @@ def post_processing():
 
 
 
-    noise_sampler = np.load('postprocessing/test1.npy', allow_pickle=True)
+    noise_sampler = np.load('postprocessing/chp_op31_batched_wav.npy', allow_pickle=True)
 
     hop_length = len(noise_sampler)//num_examples_to_generate
 
@@ -238,10 +194,41 @@ def post_processing():
         #plt.show()
         plt.close()
 
-post_processing()
+#post_processing()
 
-#from sklearn.preprocessing import normalize
+def video_processing():
+    frame_rate = 16
+    sample_rate = 22050
 
+    generator.load_weights('SAVED_WEIGHTS/generator')
 
-#generator.load_weights('SAVED_WEIGHTS/01swaths/generator')
-#new_noise = np.load('test/track02_batched_wav.npy')
+    input_file = np.load('postprocessing/chp_op31_batched_wav.npy', allow_pickle=True)
+
+    hop_length = len(input_file) // (len(input_file)*noise_dim//sample_rate)
+
+    sample_indices = range(0, len(input_file), hop_length)
+
+    edited_input = np.array([input_file[i] for i in sample_indices])
+
+    test_predictions = generator(edited_input, training=False)
+    test_predictions = tf.linalg.normalize(test_predictions, axis=3)[0]
+    test_predictions = tf.math.abs(test_predictions)
+
+    num_output_maps = len(test_predictions)//num_examples_to_generate
+
+    img_array = []
+    for i in range(num_output_maps):
+        fig = plt.figure(figsize=(4, 4))
+
+        for j in range(num_examples_to_generate):
+            plt.subplot(4, 4, j+1)
+            plt.imshow(test_predictions[j+i*num_examples_to_generate, :, :, :])
+            plt.axis('off')
+
+            img_array.append(test_predictions[j+i*num_examples_to_generate, :, :, :])
+
+        plt.savefig('post_training_examples/test{:03}.png'.format(i))
+        plt.close()
+        np.save('img_array.npy', img_array)
+
+video_processing()
