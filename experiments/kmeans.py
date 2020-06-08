9e1c89397c9126048d3872b4c286fc37c838d99d
Anjana Saravanan <65838949+affengoettin@users.noreply.github.com>
2020-06-08 11:13:09 -0700

    Add files via upload

--- experiments/kmeans.py
+++ experiments/kmeans.py
@@ -2,6 +2,8 @@
 Do k-means clustering on a bunch of input WAV data, THEN fit the data to the
 chopin scherzo.
 
+TRYING WITH MFCCS rather than RMS. gah
+
 """
 
 import librosa
@@ -15,103 +17,111 @@ import imageio
 
 import moviepy.editor as mpy
 
+MODE = 'mfcc'
+
+input_dir = 'input_wavs'
+
+im_dim = 140
+
+anim_file = 'test.gif'
+
+test_file = 'chp_op31.wav'
+
+sample_rate = 32768
+
+
 def batch(array, batch_size):
     batched_array = []
     for i in range(0, len(array), batch_size):
         batched_array.append(array[i:i+batch_size])
     return batched_array
 
-sample_rate = 32768
 
-input_dir = 'input_wavs'
+def kmeans_fit():
 
-input_red, input_green, input_blue = [], [], []
+    input_red, input_green, input_blue = [], [], []
 
-for input_file in os.listdir(input_dir):
-    print('Extracting from', input_file)
+    for input_file in os.listdir(input_dir):
+        print('Extracting from', input_file)
 
-    input_wav = librosa.load(os.path.join(input_dir, input_file), sr=sample_rate)[0]
-    input_chromagram = librosa.feature.chroma_cens(input_wav, n_chroma=3, sr=sample_rate)
+        input_wav = librosa.load(os.path.join(input_dir, input_file), sr=sample_rate)[0]
 
-    temp_red = batch(input_chromagram[0], 64)
-    temp_green = batch(input_chromagram[1], 64)
-    temp_blue = batch(input_chromagram[2], 64)
+        print('Original WAV length is', len(input_wav))
+        if MODE == 'chromagram':
+            input_feature = librosa.feature.chroma_cens(input_wav, n_chroma=3, sr=sample_rate)
+        elif MODE == 'mfcc':
+            input_feature = librosa.feature.mfcc(input_wav, n_mfcc=3, sr=sample_rate)
 
-    temp_red.pop()
-    temp_green.pop()
-    temp_blue.pop()
+        temp_red = batch(input_feature[0], 64)
+        temp_green = batch(input_feature[1], 64)
+        temp_blue = batch(input_feature[2], 64)
 
-    input_red.extend(temp_red)
-    input_green.extend(temp_green)
-    input_blue.extend(temp_blue)
+        temp_red.pop()
+        temp_green.pop()
+        temp_blue.pop()
 
-#input_file = 'input_wavs/input_wav_01.wav'
-#input_wav, sr = librosa.load(input_file, sr=sample_rate)
-#input_chromagram = librosa.feature.chroma_cens(input_wav, n_chroma=3, sr=sample_rate)
-
-#input_red = batch(input_chromagram[0], 64)
-#input_green = batch(input_chromagram[1], 64)
-#input_blue = batch(input_chromagram[2], 64)
-
-#input_red.pop()
-#input_green.pop()
-#input_blue.pop()
-
-kmeans_red = KMeans(n_clusters=10)
-kmeans_red.fit(input_red)
-
-kmeans_green = KMeans(n_clusters=10)
-kmeans_green.fit(input_green)
-
-kmeans_blue = KMeans(n_clusters=10)
-kmeans_blue.fit(input_blue)
-
-print('K-means fitting complete.')
+        input_red.extend(temp_red)
+        input_green.extend(temp_green)
+        input_blue.extend(temp_blue)
 
 
-test_file = 'chp_op31.wav'
+    kmeans_red = KMeans(n_clusters=10)
+    kmeans_red.fit(input_red)
 
-raw_wav, sr = librosa.load(test_file, sr=sample_rate)
+    kmeans_green = KMeans(n_clusters=10)
+    kmeans_green.fit(input_green)
 
-print('Test input loaded.')
+    kmeans_blue = KMeans(n_clusters=10)
+    kmeans_blue.fit(input_blue)
 
-#print('raw wav shape', raw_wav.shape)
+    print('K-means fitting complete.')
 
-test_chromagram = librosa.feature.chroma_cens(raw_wav, n_chroma=3, sr=sample_rate)
-# current hop length is 512, or 32786//64. 64 chroma per second.
+    return kmeans_red, kmeans_green, kmeans_blue
 
-# we can batch up the chromagram for each bin into segments of 64.
+def generate_predictions(kmeans_red, kmeans_green, kmeans_blue):
 
-test_red = batch(test_chromagram[0], 64)
-test_green = batch(test_chromagram[1], 64)
-test_blue = batch(test_chromagram[2], 64)
+    raw_wav, sr = librosa.load(test_file, sr=sample_rate)
+
+    print('Test input loaded.')
+
+    if MODE == 'chromagram':
+        test_feature = librosa.feature.chroma_cens(raw_wav, n_chroma=3, sr=sample_rate)
+    elif MODE == 'mfcc':
+        test_feature = librosa.feature.mfcc(raw_wav, n_mfcc=3, sr=sample_rate)
+    # current hop length is 512, or 32786//64. 64 chroma per second.
+
+    # we can batch up the chromagram for each bin into segments of 64.
+
+    test_red = batch(test_feature[0], 64)
+    test_green = batch(test_feature[1], 64)
+    test_blue = batch(test_feature[2], 64)
 
 
-test_red.pop()
-test_green.pop()
-test_blue.pop()
+    test_red.pop()
+    test_green.pop()
+    test_blue.pop()
 
-# now we k-means the shit out of this. try 10 bins first.
+    test_red = np.array(test_red).astype(np.float64)
+    test_green = np.array(test_green).astype(np.float64)
+    test_blue = np.array(test_blue).astype(np.float64)
 
-red_predictions = [kmeans_red.predict(red_elem.reshape(1, -1))[0] for red_elem in test_red]
-green_predictions = [kmeans_green.predict(green_elem.reshape(1, -1))[0] for green_elem in test_green]
-blue_predictions = [kmeans_blue.predict(blue_elem.reshape(1, -1))[0] for blue_elem in test_blue]
+    red_predictions = [kmeans_red.predict(red_elem.reshape(1, -1))[0] for red_elem in test_red]
+    green_predictions = [kmeans_green.predict(green_elem.reshape(1, -1))[0] for green_elem in test_green]
+    blue_predictions = [kmeans_blue.predict(blue_elem.reshape(1, -1))[0] for blue_elem in test_blue]
 
-print('Predictions generated.')
+    print('Predictions generated.')
 
-zipped_predictions = zip(red_predictions, green_predictions, blue_predictions)
+    zipped_predictions = zip(red_predictions, green_predictions, blue_predictions)
 
-generated_colors = []
+    generated_colors = []
 
-for r, g, b in zipped_predictions:
-    scaled_r, scaled_g, scaled_b = r*25, g*25, b*25
-    generated_colors.append((scaled_r, scaled_g, scaled_b))
+    for r, g, b in zipped_predictions:
+        scaled_r, scaled_g, scaled_b = r*25, g*25, b*25
+        generated_colors.append((scaled_r, scaled_g, scaled_b))
 
-im_dim = 140
+    return generated_colors
 
-anim_file = 'test.gif'
-
-def make_gif():
+def make_gif(generated_colors):
     print('Compiling images...')
     writer = imageio.get_writer(anim_file, mode='I', fps=1)
 
@@ -125,8 +135,6 @@ def make_gif():
 
     display.Image(filename=anim_file)
 
-make_gif()
-
 def compile_video():
     print('Compiling video and audio...')
     song = mpy.AudioFileClip(test_file)
@@ -136,4 +144,7 @@ def compile_video():
     clip.write_videofile('test_video.mp4', codec='mpeg4')
 
 
+red_clusters, green_clusters, blue_clusters = kmeans_fit()
+color_data = generate_predictions(red_clusters, green_clusters, blue_clusters)
+make_gif(color_data)
 compile_video()
